44b7865096f518b9e5cdceba480fb6356994f808
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-22 11:14:53 +0100

    add Socket.IO to server

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const express = require('express');
-const app = express();
+const socket = require('socket.io');
 
-// Middleware to catch all requests and return "Not found" message
-app.use((req, res) => {
-  res.status(404).send('Not found');
+const app = express();
+const port = 8000;
+
+// Start the server
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Set up Socket.IO
+const io = socket(server);
+
+// Initialize the users array
+let tasks = [];
+
+// Handle a new connection to the server
+io.on('connection', (socket) => {
+  console.log(`New client connected: ${socket.id}`);
+
+  // send tasks data to the new user
+  socket.emit('updateData', tasks);
+
+  // handle addTask event
+  socket.on('addTask', (task) => {
+    // add task to tasks array
+    tasks.push(task);
+
+    // emit addTask event to other sockets
+    socket.broadcast.emit('addTask', task);
+  });
+
+  // // Handle disconnection from the client
+  // socket.on('disconnect', () => {
+  //   console.log(`Client disconnected: ${socket.id}`);
+
+  // handle removeTask event
+  socket.on('removeTask', (id) => {
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    if (taskIndex !== -1) {
+      const [removedTask] = tasks.splice(taskIndex, 1);
+      socket.broadcast.emit('removeTask', removedTask.id);
+    }
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found' });
 });
